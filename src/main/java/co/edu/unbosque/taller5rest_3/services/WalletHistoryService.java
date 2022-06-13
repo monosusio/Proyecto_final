a1f354e3d97d81663be2ef1b2384b2ea0fa78765
santiagocm12 <scortesmo@unbosque.edu.co>
2022-06-13 09:20:13 -0500

    Html

--- src/main/java/co/edu/unbosque/taller5rest_3/services/WalletHistoryService.java
+++ src/main/java/co/edu/unbosque/taller5rest_3/services/WalletHistoryService.java
@@ -66,7 +66,7 @@ public class WalletHistoryService {
     public Connection connect() throws SQLException {
         String DB_URL = "jdbc:postgresql://localhost/postgres";
         String USER = "postgres";
-        String PASS = "monosusio";
+        String PASS = "minicraftteo";
 
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
